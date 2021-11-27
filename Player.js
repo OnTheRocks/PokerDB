8cd4273469379d0712f7c44ae03de9f4bd454d9e
OnTheRocks <nathan_huber@yahoo.com>
2021-11-27 01:23:45 -0600

    Updated Schema

--- Player.js
+++ Player.js
@@ -1,9 +1,39 @@
 const mongoose = require("mongoose")
 
 const playerSchema = new mongoose.Schema({
-  name: String,
-
-
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  nickName: String,
+  phone: {
+    type: String,
+    required: true,
+    minlength: 12,
+    maxlength: 12,
+  } ,
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  } ,
+  location: String,
+  notes: String,
+  createdAT: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+  
+  
 })
 
 module.exports = mongoose.model("Player", playerSchema)
