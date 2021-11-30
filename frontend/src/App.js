7b8a2f94e3922d5746e474615aa7bf6e864d5aac
OnTheRocks <nathan_huber@yahoo.com>
2021-11-30 00:46:33 -0600

    Work on Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,7 @@ import Navbar from './Components/Navbar';
 function App() {
   return (
     <div>
-
       <Navbar />
-      {/* <Routes>
-        
-        <Route path="/">Home</Route>
-        <Route path="/Player">Players</Route>
-        <Route path="/Create">Create New Player</Route>
-      </Routes> */}
     </div>
   );
 }
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -3,31 +3,28 @@ import {Link} from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">PokerDB</a>
+        <a class="navbar-brand" href="/">PokerDB</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Players</a>
+              <a class="nav-link" href="/players">Players</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Create New Player</a>
+              <a class="nav-link" href="/Create">Create New Player</a>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-    // <nav className="navbar bg-dark container">
-    //   <h3><Link to="/">Home</Link></h3>
-    //   <h3><Link to="/players">Players</Link></h3>
-    //   <h3><Link to="/create">Create New Players</Link></h3>
-    // </nav>
+
+
   )
 }
