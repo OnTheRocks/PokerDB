d4a346633ca067bdf691e2eeca98c82423d641ce
OnTheRocks <nathan_huber@yahoo.com>
2021-12-03 17:30:27 -0600

    Fixed NavBar Routes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {BrowserRouter as Routes, Route} from "react-router-dom"
+import {BrowserRouter as Router, Switch, Routes, Route} from "react-router-dom"
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
+      <Routes>
+        <Route>
+          //Players
+        </Route>
+        <Route>
+          //Create New Players
+        </Route>
+      </Routes>
+
+    </Router>
+   
+      
+    
   );
 }
 
