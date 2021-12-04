7c2e47fa810378c7db9f80426cb6efe89fd2ee31
OnTheRocks <nathan_huber@yahoo.com>
2021-12-04 01:26:58 -0600

    added routes to sever.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 mongoose.connect("mongodb://localhost/PokerDB", () => { console.log("Connected to PokerDB") }, e => console.error(e))
 
-//require route 
+app.use("/", require("./routes/playerRoutes"))
 
 app.listen(3500, function() {
   console.log("express server is running on port 3500");
