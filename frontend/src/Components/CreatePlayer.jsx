e2e6b380fb260de1c3f2a4323334209323383dd7
OnTheRocks <nathan_huber@yahoo.com>
2021-12-04 00:09:46 -0600

    Created New Player form.

--- frontend/src/Components/CreatePlayer.jsx
+++ frontend/src/Components/CreatePlayer.jsx
@@ -5,15 +5,32 @@ function CreatePlayer() {
     <div className="container">
       <h1>Create New Player</h1>
       <form>
-        <div className="mb-3">
+        <div className="mb-3 form-group">
           <label for="firstName" className="form-label">First Name</label>
-          <input id="firstName" className="form-control"></input>
-          <label for="lastName" className="form-label">Last Name</label>
-          <input id="lastName" className="form-control"></input>
-          <label for="nickName" className="form-label">Nickname</label>
-          <input id="nickName" className="form-control"></input>
+          <input id="firstName" name="firstName" className="form-control"></input>
 
+          <label for="lastName" className="form-label">Last Name</label>
+          <input id="lastName" name="lastName" className="form-control"></input>
+
+          <label for="nickName" className="form-label">Nickname</label>
+          <input id="nickName" name="nickName" className="form-control"></input>
+
+          <label for="phone" className="form-label">Nickname</label>
+          <input id="phone" name="nickName" className="form-control"></input>
+
+          <label for="email" class="form-label">Email address</label>
+          <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp"></input>
+          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+
+          <label for="location" className="form-label">Location</label>
+          <input id="location" name="location" className="form-control"></input>
+
+          <label for="notes" className="form-label">Notes</label>
+          <textarea id="notes" name="notes" className="form-control"></textarea>
         </div>
+          
+          <button type="button" class="btn btn-outline-success">Add Player</button>
+
       </form>
     </div>
   )
